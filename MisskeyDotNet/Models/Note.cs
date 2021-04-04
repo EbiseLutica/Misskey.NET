940e90975be9b7d25751af9660adb1c70ebc1540
Xeltica <xeltica@gmail.com>
2021-04-04 18:38:18 +0900

    feat: Add POCO for Notes and Users

---
+++ MisskeyDotNet/Models/Note.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+namespace MisskeyDotNet
+{
+    public class Note
+    {
+        public string Id { get; set; } = "";
+        public DateTime CreatedAt { get; set; }
+        public string UserId { get; set; } = "";
+        public User User { get; set; } = null!;
+        public string? Text { get; set; }
+        public string? Cw { get; set; }
+        public string Visibility { get; set; } = "public";
+        public bool LocalOnly { get; set; }
+        public string[]? VisibleUserIds { get; set; }
+        public bool ViaMobile { get; set; }
+        public int RenoteCount { get; set; }
+        public int RepliesCount { get; set; }
+        public Dictionary<string, int> Reactions { get; set; } = new Dictionary<string, int>();
+        // public string Tags { get; set; }
+        // public string Emojis { get; set; }
+        // public string FileIds { get; set; }
+        // public string Files { get; set; }
+        // public string ReplyId { get; set; }
+        // public string RenoteId { get; set; }
+        // channelId { get; set; }
+        // channel { get; set; }
+        //     id { get; set; }
+        //     name { get; set; }
+        // }  { get; set; }
+        // mentions { get; set; }
+        public string? Uri { get; set; }
+        public string? Url { get; set; }
+        public string? _featuredId_ { get; set; }
+        public string? _prId_ { get; set; }
+    }
+}
